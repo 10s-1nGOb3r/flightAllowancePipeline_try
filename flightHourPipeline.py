bf6e9df30bef288703f028e36b572face417d98f
Rizkan Al Maududy <rizkan.almaududy@gmail.com>
2026-03-29 21:47:15 +0700

    Enhance flight hour data processing and export
    Added data cleansing and formatting functions to process flight hour data. Implemented aggregation and export of cleaned data to CSV files.

--- flightHourPipeline.py
+++ flightHourPipeline.py
@@ -2,14 +2,22 @@ import pandas as pd
 import numpy as np
 import os
 
+#Creating file path for file reading and file exporting
 script_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(script_dir,"input","dfsFhAllowance.csv")
 save_at = os.path.join(script_dir,"output","flightHourAllowance.csv")
 save_at2 = os.path.join(script_dir,"output","detailFlightHourAllowance.csv")
 
+#Reading the files generated from AIMS
+#the required fields such as
+#DATE,FLT,TYPE,REG,AC,DEP,ARR,STD,STA,ATD,ATA,BLOCK,CREW #,Crew
 df = pd.read_csv(file_path,sep=";")
 
+#A function that is used for cleansing data and formatting
 def dataCleansingFormatting():
+    #Some fields are being reformated and cleansed
+    #Also some fileds are made by using logic
+    #such as DATE,MONTH,YEAR,RANK,fataPayment,keyCockpitSmtFda
     df["DATE"] = pd.to_datetime(df["DATE"], format="%d/%m/%Y", errors="coerce")
     df["DATE"] = df["DATE"].ffill()
     df["MONTH"] = df["DATE"].dt.month
@@ -20,6 +28,10 @@ def dataCleansingFormatting():
         df[field] = df[field].ffill()
         df[field] = df[field].astype(str)
 
+    #A condition to determine rank of a crew in one flight
+    #The raw data is like -(CPT) 303421
+    #Its about how we extract that "CPT" character
+    #This rank will be used on the followed field "fataPayment"
     conditions = [
             df["Crew"].str.contains("CPT", na=False),
             df["Crew"].str.contains("FO", na=False),
@@ -30,6 +42,13 @@ def dataCleansingFormatting():
 
     df["RANK"] = np.select(conditions,choices,default="0")
 
+    #A condition to define whether a flight will be counted
+    #into first term of FATA payment or second term of FATA payment
+    #FYI, in Citilink Indonesia the cockpit crew being paid into
+    #2 terms of FATA payment
+    #01st term is counted from 01st date to the 15th
+    #02nd term is counted as the rest of the month
+    #For cabin crew are being paid in one term of payment
     conditions3 = [(df["DATE"].dt.day < 16) & (df["RANK"] == "CPT"),
                    (df["DATE"].dt.day >= 16) & (df["RANK"] == "CPT"),
                    (df["DATE"].dt.day < 16) & (df["RANK"] == "FO"),
@@ -47,6 +66,10 @@ def dataCleansingFormatting():
     df["blockDec"] = df["BLOCK"].dt.total_seconds() / 3600
     df["blockDec"] = df["blockDec"].round(2)
 
+    #A condition to extract ID number for any crew in the dataset
+    #The raw data is like -(CPT) 303421
+    #Its about how to extract "303421" into 
+    #our field called "Crew" 
     conditions2 = [
             df["Crew"].str.contains("CPT", na=False),
             df["Crew"].str.contains("FA1", na=False),
@@ -66,16 +89,25 @@ def dataCleansingFormatting():
 
 dataCleansingFormatting()
 
+#After the data being cleaned up and formatted 
+#Next step is aggregation based on YEAR,MONTH,fataPayment
+#Rank,Crew
+#to determine the total flight hours 
+#Which this will calculate about how much anycrew would get 
+#for its allowance
 df2 = df[["YEAR","MONTH","fataPayment","RANK","Crew","blockDec"]]
 df2 = df2.groupby(["YEAR","MONTH","fataPayment","RANK","Crew"]).agg(
     totalFlightHour = ("blockDec","sum")
 ).reset_index()
 
+#Converting the format of flight hour
+#from decimal form to something like 52h 12m (52 hours 12 minutes)
 df2["totalFlightHour"] = df2["totalFlightHour"].round(2)
 total_minutes = (df2["totalFlightHour"] * 60).round()
 hours = (total_minutes // 60).astype("Int64")
 minutes = (total_minutes % 60).astype("Int64")
 df2["totalFlightHour HR:MM"] = hours.astype(str) + "h " + minutes.astype(str).str.zfill(2) + "m"
 
+#Exporting files from the pipeline to a folder called "output"
 df.to_csv(save_at2,sep=";",index=False)
 df2.to_csv(save_at,sep=";",index=False)
