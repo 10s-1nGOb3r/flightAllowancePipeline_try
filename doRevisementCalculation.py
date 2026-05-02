1aaea51e2c9212074d18b3cd6544e6c65235a211
Rizkan Al Maududy <rizkan.almaududy@gmail.com>
2026-05-02 14:55:54 +0700

    Implement time validation and unassigned duty detection
    Added datetime functionality for time validation and filtering.

--- doRevisementCalculation.py
+++ doRevisementCalculation.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
 import os
+from datetime import datetime
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(script_dir,"input","smtMealsFdaCockpit.csv")
 file_path2 = os.path.join(script_dir,"input","smtMealsResCabin.csv")
 file_path3 = os.path.join(script_dir,"input","stationDb.csv")
+file_path4 = os.path.join(script_dir,"input","unassignedDutyLabel.csv")
 save_at = os.path.join(script_dir,"output","detailDailyRestForDoRevisement.csv")
 
 df = pd.read_csv(file_path,sep=";")
@@ -78,6 +80,62 @@ df3["endOffset"] = pd.to_timedelta(df3["endOffset"])
 df3["ataOnLocalTime"] = df3["signOffDate"] + df3["endOffset"]
 df3["ataOnLocalTime"] = df3["ataOnLocalTime"] - pd.Timedelta(minutes=30)
 df3["ataOnLocalTime"] = df3["ataOnLocalTime"] + df3["transitionArr"]
+
+#timeNow = datetime.now()
+#If you need a certain time filtering, please activate 
+#the script below, 
+#deactivate scipt "timeNow = datetime.now()" above 
+#and edit timeNow on the first line
+timeNow = "01/04/2026"
+timeNow = datetime.strptime(timeNow, "%d/%m/%Y")
+timeNow = timeNow.date()
+currentMonth = timeNow.month
+if currentMonth == 1:
+    currentMonth = 12
+else:
+    currentMonth = currentMonth - 1
+
+currentYear = timeNow.year
+if currentMonth == 12:
+    currentYear = currentYear - 1
+
+df3["monthValidation"] = np.where((df3["beginOnLocalTime"].dt.month == currentMonth) & (df3["beginOnLocalTime"].dt.year == currentYear),1,0)
+
+df3["dutyFieldForDayOffDetection"] = df3["Duty"].str.replace(" ", "")
+
+df5 = pd.read_csv(file_path4,sep=";")
+
+df3 = pd.merge(df3,df5[['dutyLabelUnassigned','unassignedValidation']].drop_duplicates(subset=['dutyLabelUnassigned']),left_on='dutyFieldForDayOffDetection',right_on='dutyLabelUnassigned',how='left')
+df3 = df3.drop(columns=['dutyLabelUnassigned'])
+df3["unassignedValidation"] = df3["unassignedValidation"].fillna("0") 
+df3["unassignedValidation"] = df3["unassignedValidation"].astype(int)
+
+conditions4 = [(df3["monthValidation"] == 1) & (df3["dutyFieldForDayOffDetection"].str.contains("D/O", na=False, case=False)),
+               (df3["monthValidation"] == 1) & (df3["dutyFieldForDayOffDetection"].str.contains("DO01", na=False, case=False)),
+               (df3["monthValidation"] == 1) & (df3["dutyFieldForDayOffDetection"].str.contains(">OFF", na=False, case=False))
+]
+
+choices4 = [1,1,1]
+
+df3["dayoffValidation"] = np.select(conditions4,choices4,default=0)
+
+df3["ataInDecimal"] = (df3["ataOnLocalTime"] - df3["ataOnLocalTime"].dt.normalize()) / pd.Timedelta(hours=1)
+df3["ataInDecimal"] = df3["ataInDecimal"].round(2)
+
+conditions3 = [(df3["monthValidation"] == 1) & (df3["unassignedValidation"] == 0) & (df3["dayoffValidation"] != 1),
+               (df3["monthValidation"] == 1) & (df3["unassignedValidation"] == 1) & (df3["dayoffValidation"] == 1) & (df3["ataInDecimal"].shift(1) >= 22.00) & (df3["dutyFieldForDayOffDetection"].shift(1) != "Base:"),
+]
+
+choices3 = [1,1]
+
+df3["assignableDayValidation"] = np.select(conditions3,choices3,default=0)
+
+conditions4 = [(df3["monthValidation"] == 1) & (df3["unassignedValidation"] == 0) & (df3["dayoffValidation"] != 1),
+]
+
+choices4 = [1]
+
+df3["assignableDayValidationForComparisson"] = np.select(conditions4,choices4,default=0)
 
 df3.info()
 
